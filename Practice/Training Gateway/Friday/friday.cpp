7316997c75eb6cbef66d1e63a96c7220935e24af
Daniel Wang <daniel22373@gmail.com>
2019-07-12 16:50:02 -0400

    Worked on "Friday" problem

--- Practice/Training Gateway/Friday/friday.cpp
+++ Practice/Training Gateway/Friday/friday.cpp
@@ -12,21 +12,8 @@ LANG: C++
 using namespace std;
 
 // input: days from beginning of 1900; output: weekday of that day
-string daysToWeekday(int n) {
-	
-}
-
-int yearsToDays(int n) {
-	int days = 0;
-	for (int i = 1900; i < 1900 + n; i++) {
-		if (i == 2000 || ((i % 4 == 0) && (i % 100 != 0))) {
-			days += 366;
-		} else {
-			days += 365;
-		}
-	}
-
-	return days;
+string daysToWeekday(int n, map<int, string> key) {
+	return key[(n % 7)];
 }
 
 
@@ -51,7 +38,57 @@ int main() {
 	thirteenths["Fri"] = 0;
 	thirteenths["Sat"] = 0;
 	thirteenths["Sun"] = 0;
-	cout << thirteenths["Mon"] << endl;
+
+	// key returning weekday based on days from beginning of 1900 % 7
+	map<int, string> weekdayKey;
+	weekdayKey[0] = "Sun";
+	weekdayKey[1] = "Mon";
+	weekdayKey[2] = "Tue";
+	weekdayKey[3] = "Wed";
+	weekdayKey[4] = "Thu";
+	weekdayKey[5] = "Fri";
+	weekdayKey[6] = "Sat";
+
+	int days = 0;
+	for (int i = 1900; i < 1900 + years; i++) {
+		if (i == 2000 || ((i % 4 == 0) && (i % 100 != 0))) {
+			thirteenths[daysToWeekday(days + 13, weekdayKey)] += 1;
+			thirteenths[daysToWeekday(days + 44, weekdayKey)] += 1;
+			thirteenths[daysToWeekday(days + 63, weekdayKey)] += 1;
+			thirteenths[daysToWeekday(days + 94, weekdayKey)] += 1;
+			thirteenths[daysToWeekday(days + 124, weekdayKey)] += 1;
+			thirteenths[daysToWeekday(days + 155, weekdayKey)] += 1;
+			thirteenths[daysToWeekday(days + 175, weekdayKey)] += 1;
+			thirteenths[daysToWeekday(days + 206, weekdayKey)] += 1;
+			thirteenths[daysToWeekday(days + 237, weekdayKey)] += 1;
+			thirteenths[daysToWeekday(days + 267, weekdayKey)] += 1;
+			thirteenths[daysToWeekday(days + 298, weekdayKey)] += 1;
+			thirteenths[daysToWeekday(days + 328, weekdayKey)] += 1;
+			days += 366;
+		} else {
+			thirteenths[daysToWeekday(days + 13, weekdayKey)] += 1;
+			thirteenths[daysToWeekday(days + 44, weekdayKey)] += 1;
+			thirteenths[daysToWeekday(days + 62, weekdayKey)] += 1;
+			thirteenths[daysToWeekday(days + 93, weekdayKey)] += 1;
+			thirteenths[daysToWeekday(days + 123, weekdayKey)] += 1;
+			thirteenths[daysToWeekday(days + 154, weekdayKey)] += 1;
+			thirteenths[daysToWeekday(days + 174, weekdayKey)] += 1;
+			thirteenths[daysToWeekday(days + 205, weekdayKey)] += 1;
+			thirteenths[daysToWeekday(days + 236, weekdayKey)] += 1;
+			thirteenths[daysToWeekday(days + 266, weekdayKey)] += 1;
+			thirteenths[daysToWeekday(days + 297, weekdayKey)] += 1;
+			thirteenths[daysToWeekday(days + 327, weekdayKey)] += 1;
+			days += 365;
+		}
+	}
+
+	cout << "Mon: " << thirteenths["Mon"] << " ";
+	cout << "Tue: " << thirteenths["Tue"] << " ";
+	cout << "Wed: " << thirteenths["Wed"] << " ";
+	cout << "Thu: " << thirteenths["Thu"] << " ";
+	cout << "Fri: " << thirteenths["Fri"] << " ";
+	cout << "Sat: " << thirteenths["Sat"] << " ";
+	cout << "Sun: " << thirteenths["Sun"] << " ";
 
 	ofstream writer("output.txt");
 	if (writer.is_open()) {
