4c13b1826833af386faa99c4dfc4fef298e2bd1f
Daniel Wang <daniel22373@gmail.com>
2019-07-15 14:45:07 -0400

    Worked on "Transform", solved cases with even square lengths

--- Practice/Training Gateway/Transformations/transform.cpp
+++ Practice/Training Gateway/Transformations/transform.cpp
@@ -42,10 +42,66 @@ vector<vector<char>> rotateNinety(vector<vector<char>> sqr) {
 	}
 	// scenario 1: square length is odd
 	if (sqr.size() % 2 == 1) {
-		int bound = floor(sqr.size()/2);
+		int bound = floor(sqr.size() / 2);
 		for (int i = -bound; i <= bound; i++) {
 			for (int j = -bound; j <= bound; j++) {
 				newSqr[j+bound][-i+bound] = sqr[i+bound][j+bound];
+			}
+		}
+	} // scenario 2: square length is even
+	else {
+		// int bound = sqr.size() / 2;
+		// for (int i = -bound; i <= bound; i++) {
+		// 	if (i != 0) {
+		// 		cout << "i: " << i << " ";
+		// 		for (int j = -bound; j <= bound; j++) {
+		// 			if (j != 0) {
+		// 				cout << "j: " << j << endl;
+		// 				int newI = -10;
+		// 				int newJ = -10;
+		// 				// if (i < 0)
+		// 				// 	newI = i + bound;
+		// 				// else
+		// 				// 	newI = i + bound - 1;
+		// 				// if (j < 0)
+		// 				// 	newJ = j + bound;
+		// 				// else
+		// 				// 	newJ = j + bound - 1;
+		// 				// cout << "newI: " << newI << " " << "newJ: " << newJ << endl;
+		// 				if (i < 0 && j < 0) {
+		// 					newSqr[j + bound][-i + bound] = sqr[i + bound][j + bound];
+		// 				} else if (i > 0 && j > 0) {
+		// 					newSqr[newJ = j + bound - 1][-i + bound - 1] = sqr[i + bound - 1][newJ = j + bound - 1];
+		// 				} else if (i < 0 && j > 0) {
+		// 					newSqr[newJ = j + bound - 1][-i + bound] = sqr[i + bound][newJ = j + bound - 1];
+		// 				} else if (i > 0 && j < 0){
+		// 					newSqr[j + bound][-i + bound - 1] = sqr[i + bound - 1][j + bound];
+		// 				}
+		// 			}
+		// 		}
+		// 	}
+		// }
+	}
+
+	return newSqr;
+}
+
+vector<vector<char>> rotateOneEighty(vector<vector<char>> sqr) {
+	// initializing output square
+	vector<vector<char>> newSqr;
+	for (int i = 0; i <sqr.size(); i++) {
+		vector<char> line;
+		for (int j = 0; j < sqr.size(); j++) {
+			line.push_back('X');
+		}
+		newSqr.push_back(line);
+	}
+	// scenario 1: square length is odd
+	if (sqr.size() % 2 == 1) {
+		int bound = floor(sqr.size() / 2);
+		for (int i = -bound; i <= bound; i++) {
+			for (int j = -bound; j <= bound; j++) {
+				newSqr[-i+bound][-j+bound] = sqr[i+bound][j+bound];
 			}
 		}
 	} // scenario 2: square length is even
@@ -56,21 +112,62 @@ vector<vector<char>> rotateNinety(vector<vector<char>> sqr) {
 	return newSqr;
 }
 
-// vector<vector<char>> rotateOneEighty(vector<vector<char>> square) {
-	
-// }
+vector<vector<char>> rotateTwoSeventy(vector<vector<char>> sqr) {
+	// initializing output square
+	vector<vector<char>> newSqr;
+	for (int i = 0; i <sqr.size(); i++) {
+		vector<char> line;
+		for (int j = 0; j < sqr.size(); j++) {
+			line.push_back('X');
+		}
+		newSqr.push_back(line);
+	}
+	// scenario 1: square length is odd
+	if (sqr.size() % 2 == 1) {
+		int bound = floor(sqr.size() / 2);
+		for (int i = -bound; i <= bound; i++) {
+			for (int j = -bound; j <= bound; j++) {
+				newSqr[-j+bound][i+bound] = sqr[i+bound][j+bound];
+			}
+		}
+	} // scenario 2: square length is even
+	else {
+		
+	}
 
-// vector<vector<char>> rotateTwoSeventy(vector<vector<char>> square) {
-	
-// }
+	return newSqr;
+}
 
-// vector<vector<char>> reflectHoriz(vector<vector<char>> square) {
-	
-// }
+vector<vector<char>> reflectHoriz(vector<vector<char>> sqr) {
+	// initializing output square
+	vector<vector<char>> newSqr;
+	for (int i = 0; i <sqr.size(); i++) {
+		vector<char> line;
+		for (int j = 0; j < sqr.size(); j++) {
+			line.push_back('X');
+		}
+		newSqr.push_back(line);
+	}
+	// scenario 1: square length is odd
+	if (sqr.size() % 2 == 1) {
+		int bound = floor(sqr.size() / 2);
+		for (int i = -bound; i <= bound; i++) {
+			for (int j = -bound; j <= bound; j++) {
+				newSqr[i+bound][-j+bound] = sqr[i+bound][j+bound];
+			}
+		}
+	} // scenario 2: square length is even
+	else {
+
+	}
+
+	return newSqr;
+}
 
 int main() {
 	// reading input
-	ifstream reader("input.txt");
+	// ifstream reader("input.txt");
+	ifstream reader("transform.in");
 	int sizeSquare = -1;
 	string strSizeSquare;
 	// 2D vectors storing contents of input squares
@@ -105,6 +202,7 @@ int main() {
 	reader.close();
 
 	// printing original square to console
+	cout << "Original Square:" << endl;
 	for (int i = 0; i < sizeSquare; i++) {
 		for (int j = 0; j < sizeSquare; j++) {
 			cout << origSquare[i][j];
@@ -115,6 +213,7 @@ int main() {
 
 	// TEST: printing transformed original square to console
 	// origSquare = rotateNinety(origSquare);
+	cout << "Transformed Original Square:" << endl;
 	for (int i = 0; i < sizeSquare; i++) {
 		for (int j = 0; j < sizeSquare; j++) {
 			cout << origSquare[i][j];
@@ -124,6 +223,7 @@ int main() {
 	cout << endl;
 
 	// printing transformed square to console
+	cout << "Transformed Square:" << endl;
 	for (int i = 0; i < sizeSquare; i++) {
 		for (int j = 0; j < sizeSquare; j++) {
 			cout << transformSquare[i][j];
@@ -151,8 +251,8 @@ int main() {
 		transformCase = 4;
 	} else {
 		vector<vector<char>> combRotatedNinety = rotateNinety(reflectedHoriz);
-		vector<vector<char>> combRotatedOneEighty = rotatedOneEighty(reflectedHoriz);
-		vector<vector<char>> combRotatedTwoSeventy = rotatedTwoSeventy(reflectedHoriz);
+		vector<vector<char>> combRotatedOneEighty = rotateOneEighty(reflectedHoriz);
+		vector<vector<char>> combRotatedTwoSeventy = rotateTwoSeventy(reflectedHoriz);
 		if(equalSquare(transformSquare, combRotatedNinety)) {
 			transformCase = 5;
 		} else if(equalSquare(transformSquare, combRotatedOneEighty)) {
@@ -166,7 +266,8 @@ int main() {
 	 	}
 	} 
 
-	ofstream writer("output.txt");
+	// ofstream writer("output.txt");
+	ofstream writer("transform.out");
 	if (writer.is_open()) {
 		writer << transformCase << "\n";
 	} else {
