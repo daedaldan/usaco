3b5d16cbf26a044666e369bc09f36da30c1346dd
Daniel Wang <daniel22373@gmail.com>
2019-07-18 16:06:25 -0400

    Worked on "Combo" from Training Gateway

---
+++ Practice/Training Gateway/Combination Lock/combo.cpp
@@ -0,0 +1,88 @@
+/*
+ID: daniel25
+TASK: combo
+LANG: C++
+*/
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+void generateCombos(vector<int> a, vector<int> b, int range, set<vector<int>> & valid) {
+	for (int i = 0; i < 2; i++) {
+		for (int j = -2; j <= 2; j++) {
+			if (a[i] + j > range) {
+
+			} else if (a[i] + j < 0) {
+
+			} else {
+				a[i] += j;
+				valid.insert(a);
+			}
+
+			if (b[i] + j > range) {
+
+			} else if (b[i] + j < 0) {
+
+			} else {
+				b[i] += j;
+				valid.insert(b);
+			}
+		}
+	}
+}
+
+int main() {
+	// reading input
+	string strLockRange;
+	int lockRange;
+	string strCombo;
+	// vectors of ints with Farmer John's and master combination, respectively
+	vector<int> combo1;
+	vector<int> combo2;
+	ifstream reader("input.txt");
+	if (reader.is_open()) {
+		getline(reader, strLockRange);
+		lockRange = stoi(strLockRange);
+		getline(reader, strCombo);
+		int blank1 = strCombo.find(" ");
+		int blank2 = strCombo.find(" ", blank1+1);
+		int num1 = stoi(strCombo.substr(0, blank1));
+		int num2 = stoi(strCombo.substr(blank1, blank2));
+		int num3 = stoi(strCombo.substr(blank2));
+		combo1.push_back(num1);
+		combo1.push_back(num2);
+		combo1.push_back(num3);
+		getline(reader, strCombo);
+		blank1 = strCombo.find(" ");
+		blank2 = strCombo.find(" ", blank1+1);
+		num1 = stoi(strCombo.substr(0, blank1));
+		num2 = stoi(strCombo.substr(blank1, blank2));
+		num3 = stoi(strCombo.substr(blank2));
+		combo2.push_back(num1);
+		combo2.push_back(num2);
+		combo2.push_back(num3);
+	} else {
+		cout << "error opening input file" << endl;
+	}
+	reader.close();
+
+	set<vector<int>> validCombos;
+
+	generateCombos(combo1, combo2, lockRange, validCombos);
+
+	// writing output
+	ofstream writer("output.txt");
+	if (writer.is_open()) {
+		writer << validCombos.size() << "\n";
+	} else {
+		cout << "error opening output file" << endl;
+	}
+	writer.close();
+
+	return 0;
+}
