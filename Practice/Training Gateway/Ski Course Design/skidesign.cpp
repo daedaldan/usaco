e797b2f6dbadcf013dedf0bdeb2b30d1ffcf2022
Daniel Wang <daniel22373@gmail.com>
2019-07-20 16:05:20 -0400

    Solved "Ski Design" from Training Gateway

--- Practice/Training Gateway/Ski Course Design/skidesign.cpp
+++ Practice/Training Gateway/Ski Course Design/skidesign.cpp
@@ -7,55 +7,51 @@ LANG: C++
 #include <iostream>
 #include <string>
 #include <fstream>
-#include <set>
+#include <vector>
 #include <algorithm>
-#include <cmath>
 
 using namespace std;
 
 int main() {
 	// reading input
-	ifstream reader("input.txt");
-	// ifstream reader("skidesign.in");
+	// ifstream reader("input.txt");
+	ifstream reader("skidesign.in");
 	string line;
 	int numHills;
-	multiset<int> hillHeights;
+	vector<int> hillHeights;
 	if (reader.is_open()) {
 		getline(reader, line);
 		numHills = stoi(line);
 		for (int i = 0; i < numHills; i++) {
 			getline(reader, line);
-			hillHeights.insert(stoi(line));
+			hillHeights.push_back(stoi(line));
 		}
 	} else {
 		cout << "error opening input file" << endl;
 	}
 	reader.close();
-	
 
-	int max = *hillHeights.rbegin();
-	int min = *hillHeights.begin();
-	int cost = 0;
-	int count = 0;
-	while (max - min > 17) {
-		int needed = ceil((max - min - 17) / 2.0);
-		cost += 2 * (needed * needed);
 
-		hillHeights.erase(hillHeights.begin(), ++hillHeights.begin());
-		hillHeights.erase(--hillHeights.end(), hillHeights.end());
-		hillHeights.insert(max - needed);
-		hillHeights.insert(min + needed);
-
-		max = *hillHeights.rbegin();
-		min = *hillHeights.begin();
-		cout << hillHeights.size() << endl;
+	int minCost = 100000000;
+	for (int i = 1; i <= 83; i++) {
+		int cost = 0;
+		for (int j = 0; j < hillHeights.size(); j++) {
+			if (hillHeights[j] > i && hillHeights[j] < i + 17) {
+				continue;
+			} else if (hillHeights[j] > i + 17) {
+				cost += (hillHeights[j] - (i + 17)) * (hillHeights[j] - (i + 17));
+			} else if (hillHeights[j] < i) {
+				cost += (i - hillHeights[j]) * (i - hillHeights[j]);
+			}
+		}
+		minCost = min(minCost, cost);
 	}
 
 	// writing output
-	ofstream writer("output.txt");
-	// ofstream writer("skidesign.out");
+	// ofstream writer("output.txt");
+	ofstream writer("skidesign.out");
 	if (writer.is_open()) {
-		writer << cost << "\n";
+		writer << minCost << "\n";
 	} else {
 		cout << "error opening output file" << endl;
 	}
