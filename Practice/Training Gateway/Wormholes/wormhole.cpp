17d7f5899a3ec7c8eed701f0301e8afd0f177e61
Daniel Wang <daniel22373@gmail.com>
2019-07-21 12:23:34 -0400

    Worked on "Wormhole"

--- Practice/Training Gateway/Wormholes/wormhole.cpp
+++ Practice/Training Gateway/Wormholes/wormhole.cpp
@@ -36,23 +36,29 @@ bool duplicateHoles(holePair a, holePair b) {
 	return true;
 }
 
-void calculateWormholePairs(vector<wormhole> holes, vector<holePair> soFar, vector<vector<holePair>> & pairs) {
+void calculateWormholePairs(vector<wormhole> holes, vector<holePair> soFar, vector<vector<holePair>> & pairs, int depth) {
+	cout << "depth: " << depth << endl;
+	cout << "holes size: " << holes.size() << endl;
+	cout << "soFar size: " << soFar.size() << endl;
 	if (holes.size() == 0) {
 		pairs.push_back(soFar);
 	} else {
 		for (int i = 0; i < holes.size(); i++) {
 			for (int j = 0; j < holes.size(); j++) {
-				cout << "here" << endl;
+				cout << "hole #" << holes[i].id << " and hole #" << holes[j].id << " (i = " << i << " and j = " << j << ")" << endl;
 				holePair p;
 				p.id1 = holes[i].id;
 				p.id2 = holes[j].id;
 				if (holes[i].id != holes[j].id && find(soFar.begin(), soFar.end(), p) == soFar.end()) {
-					cout << "hi" << endl;
+					cout << " are valid" << endl;
+					cout << depth << " here" << endl;
 					soFar.push_back(p);
+					// cout << depth << " here" << endl;
 					vector<wormhole> newHoles = holes;
+
 					newHoles.erase(find(holes.begin(), holes.end(), holes[i]), ++find(holes.begin(), holes.end(), holes[i]));
 					newHoles.erase(find(holes.begin(), holes.end(), holes[j]), ++find(holes.begin(), holes.end(), holes[j]));
-					calculateWormholePairs(newHoles, soFar, pairs);
+					calculateWormholePairs(newHoles, soFar, pairs, ++depth);
 				}
 			}
 		}
@@ -84,13 +90,37 @@ int main() {
 	}
 	reader.close();
 
-
 	// calculate set of unique holePairs
 	vector<vector<holePair>> wormholePairs;
-	vector<holePair> emptyholePairs;
 
-	calculateWormholePairs(wormholes, emptyholePairs, wormholePairs);
-	cout << wormholePairs.size() << "a" << endl;
+	while (wormholePairs.size() < (wormholes.size() * (wormholes.size() - 1)) / 2) {
+		vector<wormhole> holes = wormholes;
+		vector<holePair> holePairs;
+		while (holes.size() != 0) {
+			for (int i = 0; i < holes.size(); i++) {
+				for (int j = 0; j < holes.size(); j++) {
+					holePair p;
+					p.id1 = holes[i].id;
+					p.id2 = holes[j].id;
+					if (holes[i].id != holes[j].id && find(holePairs.begin(), holePairs.end(), p) == holePairs.end()) {
+						holePairs.push_back(p);
+						holes.erase(find(holes.begin(), holes.end(), holes[i]), ++find(holes.begin(), holes.end(), holes[i]));
+						holes.erase(find(holes.begin(), holes.end(), holes[j]), ++find(holes.begin(), holes.end(), holes[j]));
+					}
+				}
+			}
+		}
+		wormholePairs.push_back(holePairs);
+	}
+
+	for (int i = 0 ; i < wormholePairs.size(); i++) {
+		cout << "Set " << i << ":" << endl;
+		for (int j = 0; j < wormholePairs[i].size(); j++) {
+			cout << "Pair " << j << ":" << endl;
+			cout << wormholePairs[i][j].id1 << ", " << wormholePairs[i][j].id2 << endl;
+		}
+	}
+	cout << wormholePairs.size() << endl;
 	// for (int i = 0; i < wormholePairs.size(); i++) {
 	// 	for (int j = 0; j < wormholePairs[i].size(); j++) {
 	// 		int start = wormholePairs[i][j][0];
