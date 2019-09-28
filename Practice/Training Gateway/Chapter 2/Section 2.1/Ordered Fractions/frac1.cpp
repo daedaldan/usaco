2e53e95262cf709a5a0adbd40fc10ef20a1a8ea8
Daniel Wang <daniel22373@gmail.com>
2019-09-28 15:37:46 -0400

    Solved The Castle, started Ordered Fractions

---
+++ Practice/Training Gateway/Chapter 2/Section 2.1/Ordered Fractions/frac1.cpp
@@ -0,0 +1,27 @@
+/*
+ID: daniel25
+TASK: taskname
+LANG: C++
+*/
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    // reading input
+    ifstream fin("input.in");
+    if (fin.is_open()) {
+
+    } else cout << "error opening input file" << endl;
+    fin.close();
+
+    // writing output
+    ofstream fout("output.out");
+    if (fout.is_open()) {
+
+    } else cout << "error opening output file" << endl;
+    fout.close();
+}
--- Practice/Training Gateway/Chapter 2/Section 2.1/The Castle/castle.cpp
+++ Practice/Training Gateway/Chapter 2/Section 2.1/The Castle/castle.cpp
@@ -12,16 +12,16 @@ using namespace std;
 
 struct component {
     int size;
-    int nodes[500][2];
+    int nodes[512][2];
 };
 
 int M, N;
 int castle[150][150];
-component components[500];
+component components[2500];
 int numComponents = 0;
 int largestComponent = -1;
 int largestCreatableRoom = -1;
-int wallToRemove[2];
+int wallToRemove[2] = {-1, 151};
 string wtrDirection;
 
 void intToWalls(int i, int row, int col) {
@@ -71,7 +71,10 @@ void populateCastle(ifstream & fin) {
 void printCastle() {
     for (int i = 0; i < N*2+1; i++) {
         for (int j = 0; j < M*2+1; j++) {
-            cout << castle[i][j] << " ";
+            if (castle[i][j] == 1)
+                cout << "X" << " ";
+            else
+                cout << "  ";
         }
         cout << endl;
     }
@@ -79,31 +82,41 @@ void printCastle() {
 }
 
 void dfs(int y, int x, component & c, int i) {
+//    cout << y / 2 + 1 << " " << (x + 1) / 2 << endl;
     // assign current node to component
-    c.nodes[i][0] = y;
-    c.nodes[i][1] = x;
-    c.size += 1;
+    if (y % 2 == 1 && x % 2 == 1) {
+        c.nodes[i][0] = y;
+        c.nodes[i][1] = x;
+    }
+    if (x % 2 == 1 && y % 2 == 1)
+        c.size += 1;
     // mark current node visited
     castle[y][x] = 8;
     // recurse on neighbor nodes and mark them visited
     for (int a = -1; a <= 1; a++) {
         for (int b = -1; b <= 1; b++) {
-            if (castle[y+a][x+b] == 0)
-                dfs(y+a, x+b, c, ++i);
+            if (castle[y+a][x+b] == 0) {
+                if (y % 2 == 1 && x % 2 == 1)
+                    dfs(y+a, x+b, c, ++i);
+                else
+                    dfs(y+a, x+b, c, i);
+            }
         }
     }
 }
 
 void floodFill() {
     // for each node, if unvisited
-    for (int i = 1; i < N*2; i++) {
-        for (int j = 1; j < M*2; j++) {
+    for (int i = 1; i < N*2; i+=2) {
+        for (int j = 1; j < M*2; j+=2) {
+//            cout << i << " " << j << endl;
             if (castle[i][j] == 0) {
                 component c = component();
                 dfs(i, j, c, 0);
                 components[numComponents] = c;
                 numComponents++;
             }
+//            cout << "here" << endl;
         }
     }
 }
@@ -111,24 +124,16 @@ void floodFill() {
 void findLargestComponent() {
     // call dfs on each unvisited node
     for (int i = 0; i < numComponents; i++) {
-        components[i].size = components[i].size / 2 + 1;
-        if (components[i].size > largestComponent)
+        if (components[i].size > largestComponent) {
             largestComponent = components[i].size;
+        }
     }
-}
-
-bool moreOptimal(int y, int x, int optimal[2]) {
-    if (x < optimal[1])
-        return true;
-    if (y < optimal[0])
-        return true;
-
-    return false;
 }
 
 void findSize(int y, int x, int & size, int copyCastle[150][150]) {
     // add current node and mark visited
-    size += 1;
+    if (y % 2 == 1 && x % 2 == 1)
+        size += 1;
     copyCastle[y][x] = 0;
     // recurse on neighbor nodes and mark them visited
     for (int a = -1; a <= 1; a++) {
@@ -138,6 +143,14 @@ void findSize(int y, int x, int & size, int copyCastle[150][150]) {
             }
         }
     }
+}
+
+bool moreOptimal(int y, int x) {
+    if (x < wallToRemove[1])
+        return true;
+    if (y > wallToRemove[0])
+        return true;
+    return false;
 }
 
 void findBreakWall() {
@@ -150,8 +163,11 @@ void findBreakWall() {
                         copyCastle[i][j] = castle[i][j];
                 int size = 0;
                 findSize(i, j, size, copyCastle);
-                size = size / 2 + 1;
                 if (size > largestCreatableRoom) {
+                    largestCreatableRoom = size;
+                    wallToRemove[0] = i;
+                    wallToRemove[1] = j;
+                } else if (size == largestCreatableRoom && moreOptimal(i, j)) {
                     largestCreatableRoom = size;
                     wallToRemove[0] = i;
                     wallToRemove[1] = j;
@@ -176,13 +192,13 @@ int main() {
     findLargestComponent();
     findBreakWall();
 
+
     if (castle[wallToRemove[0]+1][wallToRemove[1]] == 8)
         wtrDirection = "N";
     else
         wtrDirection = "E";
     wallToRemove[0] = wallToRemove[0] / 2 + 1;
-    if (wallToRemove[1] != 1)
-        wallToRemove[1] = wallToRemove[1] / 2;
+    wallToRemove[1] = (wallToRemove[1] + 1) / 2;
 
     // writing output
     ofstream fout("castle.out");
@@ -193,4 +209,6 @@ int main() {
         fout << wallToRemove[0] << " " << wallToRemove[1] << " " << wtrDirection << "\n";
     } else cout << "error opening output file" << endl;
     fout.close();
+
+    return 0;
 }
