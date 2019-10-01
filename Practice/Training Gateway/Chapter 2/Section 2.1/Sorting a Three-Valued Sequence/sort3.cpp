c2eeb114d874d7015d96bff502e20cf0995d9460
Daniel Wang <daniel22373@gmail.com>
2019-10-01 16:34:31 -0400

    Looked up answer for Sorting A Three-Valued Sequence

--- Practice/Training Gateway/Chapter 2/Section 2.1/Sorting a Three-Valued Sequence/sort3.cpp
+++ Practice/Training Gateway/Chapter 2/Section 2.1/Sorting a Three-Valued Sequence/sort3.cpp
@@ -8,142 +8,58 @@ LANG: C++
 #include <string>
 #include <fstream>
 #include <queue>
+#include <algorithm>
 
 using namespace std;
 
 int N;
-int records[1000];
-int minExchanges = 99999;
+int have[1000];
+int want[1000];
 
-struct state {
-    int exchanges = 0;
-    int nums[1000];
-};
-
-void swap(int a, int b, state & s) {
-    int temp = s.nums[a];
-    s.nums[a] = s.nums[b];
-    s.nums[b] = temp;
-    s.exchanges++;
+void swap(int a, int b, int arr[]) {
+    int temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
 }
-
-bool sorted(state s) {
-    for (int i = 0; i < N-1; i++) {
-        if (s.nums[i] > s.nums[i+1])
-            return false;
-    }
-
-    return true;
-}
-
-//int process(state s, queue<state> & unprocessed) {
-//    if (sorted(s)) {
-//        cout << "       HERE " << endl;
-//        for (int i = 0; i < N; i++)
-//            cout << s.nums[i] << endl;
-//        minExchanges = s.exchanges;
-//        return 1;
-//    }
-//
-//    for (int i = 0; i < N; i++) {
-//        for (int j = 0; j < N; j++) {
-//            state sCopy = s;
-//            if (sCopy.nums[i] != sCopy.nums[j]) {
-//                swap(i, j, sCopy);
-//                unprocessed.push(sCopy);
-//            }
-//        }
-//    }
-//
-//    return 0;
-//}
-//
-
-int capped_dfs(state & s, int lastA, int lastB, int depth) {
-    cout << depth << endl;
-    if (sorted(s)) {
-        minExchanges = s.exchanges;
-        return 1;
-    }
-
-    if (depth == 0)
-        return 0;
-
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            if (records[i] != records[j] &&
-            lastA != i &&
-            lastA != j &&
-            lastB != i &&
-            lastB != j) {
-                state sCopy = state();
-                for (int k = 0; k < N; k++)
-                    sCopy.nums[k] = records[k];
-                swap(i, j, sCopy);
-                if (capped_dfs(sCopy, i, j, depth-1) == 1)
-                    return 1;
-            }
-        }
-    }
-
-    return 0;
-}
-
-void dfs_id(state s) {
-    for (int i = 0; i < 500; i++) {
-        if (capped_dfs(s, -1, -1, i) == 1)
-            return;
-    }
-}
-
-//int dfs(state & s, int lastA, int lastB) {
-//    cout << s.exchanges << endl;
-//    if (sorted(s)) {
-//        minExchanges = s.exchanges;
-//        return 1;
-//    }
-//
-//    for (int i = 0; i < N; i++) {
-//        for (int j = 0; j < N; j++) {
-//            if (records[i] != records[j] &&
-//            lastA != i &&
-//            lastA != j &&
-//            lastB != i &&
-//            lastB != j) {
-//                state sCopy = state();
-//                for (int k = 0; k < N; k++)
-//                    sCopy.nums[k] = records[k];
-//                swap(i, j, sCopy);
-//                if (dfs(sCopy, i, j) == 1)
-//                    return 1;
-//            }
-//        }
-//    }
-//
-//    return 0;
-//}
 
 int main() {
     // reading input
     ifstream fin("sort3.in");
     if (fin.is_open()) {
         fin >> N;
-        for (int i = 0; i < N; i++)
-            fin >> records[i];
+        for (int i = 0; i < N; i++) {
+            fin >> have[i];
+            want[i] = have[i];
+        }
     } else cout << "error opening input file" << endl;
     fin.close();
 
-    state s;
+    sort(want, want+N);
+
+    // swaps correcting two elements
+    int nswap = 0;
     for (int i = 0; i < N; i++) {
-        s.nums[i] = records[i];
+        for (int j = 0; j < N; j++) {
+            if (have[i] != want[i] && have[j] != want[j]
+            && have[i] == want[j] && have[j] == want[i]) {
+                have[i] = want[i];
+                have[j] = want[j];
+                nswap++;
+            }
+        }
     }
 
-    dfs_id(s);
+    int nbad = 0;
+    for (int i = 0; i < N; i++)
+        if (have[i] != want[i])
+            nbad++;
+
+    nswap += nbad/3 * 2;
 
     // writing output
     ofstream fout("sort3.out");
     if (fout.is_open()) {
-        fout << minExchanges << "\n";
+        fout << nswap << "\n";
     } else cout << "error opening output file" << endl;
     fout.close();
 }
