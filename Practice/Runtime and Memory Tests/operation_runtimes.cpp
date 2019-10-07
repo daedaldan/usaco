7b490256a56c34bdba1c540c592086c55db2e55c
Daniel Wang <daniel22373@gmail.com>
2019-10-07 19:09:25 -0400

    Started timing operations

--- Practice/Runtime and Memory Tests/operation_runtimes.cpp
+++ Practice/Runtime and Memory Tests/operation_runtimes.cpp
@@ -1,15 +1,25 @@
 /*
 ID: daniel25
-TASK: taskname
+TASK: ride
 LANG: C++
 */
 
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <chrono>
 
 using namespace std;
 
 int main() {
-
+    int i = 0;
+    int test = 0;
+    auto start = chrono::high_resolution_clock::now();
+    while (i < 2000000000) {
+        i += 1;
+        test += 1*200;
+    }
+    auto stop = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
+    cout << duration.count() << endl;
 }
