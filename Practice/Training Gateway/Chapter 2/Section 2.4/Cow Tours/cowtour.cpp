14556f54b9f82cec8c1c0215c78a64c2997d15e7
Daniel Wang <daniel22373@gmail.com>
2019-11-23 17:02:36 -0500

    Worked on Cow Tours

--- Practice/Training Gateway/Chapter 2/Section 2.4/Cow Tours/cowtour.cpp
+++ Practice/Training Gateway/Chapter 2/Section 2.4/Cow Tours/cowtour.cpp
@@ -7,13 +7,14 @@ LANG: C++
 #include <iostream>
 #include <string>
 #include <fstream>
-#include <cmath>
+#include <math.h>
 
 using namespace std;
 
 struct pasture {
     int x;
     int y;
+    int index;
     int componentNum = -1;
     bool visited = false;
 };
@@ -43,7 +44,7 @@ void printEdges() {
 }
 
 double distance(pasture a, pasture b) {
-    return 1.0;
+    return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
 }
 
 void floodFillDFS(int pastureI) {
@@ -68,8 +69,45 @@ void floodFill() {
     }
 }
 
-double findDiameter(field f) {
+double findDiameter(int fieldPastures[], int numPastures) {
+    sort(fieldPastures, fieldPastures+numPastures);
+    double dist[150][150];
+    // start with all single edge paths
+    for (int i = 0; i < numPastures; i++) {
+        for (int j = 0; j < numPastures; j++) {
+            if (edges[fieldPastures[i]][fieldPastures[j]] == 1) {
+                dist[fieldPastures[i]][fieldPastures[j]] = distance(pastures[fieldPastures[i]], pastures[fieldPastures[j]]);
+            } else {
+                dist[fieldPastures[i]][fieldPastures[j]] = 1000000000;
+            }
+        }
+    }
 
+    // find all-pairs shortest paths
+    for (int k = 0; k < numPastures; k++) {
+        for (int i = 0; i < numPastures; i++) {
+            for (int j = 0; j < numPastures; j++) {
+                if (edges[fieldPastures[i]][fieldPastures[k]] == 1
+                 && edges[fieldPastures[j]][fieldPastures[k]] == 1) {
+                    if (dist[i][k] + dist[k][j] < dist[i][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
+                }
+            }
+        }
+    }
+
+    double distance = -1;
+    // find maximum shortest path
+    for (int i = 0; i < numPastures; i++) {
+        for (int j = 0; j < numPastures; j++) {
+            if (dist[i][j] > distance && dist[i][j] != 1000000000) {
+                distance = dist[i][j];
+            }
+        }
+    }
+
+    return distance;
 }
 
 int main() {
@@ -81,6 +119,7 @@ int main() {
         for (int i = 0; i < N; i++) {
             pasture p;
             fin >> p.x >> p.y;
+            p.index = i;
             pastures[i] = p;
         }
 
@@ -105,17 +144,30 @@ int main() {
             // pairing two pastures in each of the fields
             for (int p1 = 0; p1 < fields[i].numPastures; p1++) {
                 for (int p2 = 0; p2 < fields[j].numPastures; p2++) {
-                    // connect the two pastures in a new field
-                    field newF;
-
+                    // connect the two pastures
+                    edges[fields[i].pastures[p1].index][fields[j].pastures[p2].index] = 1;
+                    // make list of which pastures are in the new field for Floyd-Warshall
+                    int pasturesInField[150];
+                    int numPasturesInField = 0;
+                    for (int a = 0; a < fields[i].numPastures; a++) {
+                        pasturesInField[numPasturesInField] = fields[i].pastures[a].index;
+                        numPasturesInField++;
+                    }
+                    for (int a = 0; a < fields[j].numPastures; a++) {
+                        pasturesInField[numPasturesInField] = fields[j].pastures[a].index;
+                        numPasturesInField++;
+                    }
                     // check the diameter of the new field
-                    double diameter = findDiameter(newF);
+                    double diameter = findDiameter(pasturesInField, numPasturesInField);
                     // if new diameter is smaller than existing minimum, record it;
+                    cout << diameter << endl;
                     if (diameter < minDiameter) {
                         minDiameter = diameter;
                         minPastureA = fields[i].pastures[p1];
                         minPastureB = fields[j].pastures[p2];
                     }
+                    // unconnect the two pastures
+                    edges[fields[i].pastures[p1].index][fields[j].pastures[p2].index] = 0;
                 }
             }
         }
