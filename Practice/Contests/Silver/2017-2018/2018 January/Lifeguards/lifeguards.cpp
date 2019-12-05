5b7b21e8101679ec585ec845dd0e711292dc5eaa
Daniel Wang <daniel22373@gmail.com>
2019-12-05 17:20:05 -0500

    Tried debugging Lifeguards

--- Practice/Contests/Silver/2017-2018/2018 January/Lifeguards/lifeguards.cpp
+++ Practice/Contests/Silver/2017-2018/2018 January/Lifeguards/lifeguards.cpp
@@ -2,15 +2,13 @@
 #include <string>
 #include <fstream>
 #include <set>
+#include <algorithm>
 
 using namespace std;
 
 struct endpoint {
-    long time;
+    long t;
     long index;
-    bool operator<(endpoint other) const {
-        return time < other.time;
-    }
 };
 
 long N;
@@ -18,6 +16,8 @@ endpoint endpoints[200000];
 set<long> working;
 long alone[100000];
 long actualTimeCovered = 0;
+
+bool cmp(endpoint a, endpoint b) { return a.t < b.t; }
 
 int main() {
     // reading input
@@ -27,8 +27,8 @@ int main() {
         for (long i = 0; i < N; i++) {
             endpoint s;
             endpoint e;
-            fin >> s.time;
-            fin >> e.time;
+            fin >> s.t;
+            fin >> e.t;
             s.index = i;
             e.index = i;
             endpoints[2*i] = s;
@@ -37,25 +37,25 @@ int main() {
     } else cout << "error opening input file" << endl;
     fin.close();
 
-    sort(endpoints, endpoints + 2 * N);
+    sort(endpoints, endpoints + 2 * N, cmp);
 
     long last = 0;
     for (long i = 0; i < 2 * N; i++) {
         if (working.size() == 1) {
-            alone[*working.begin()] += endpoints[i].time - last;
+            alone[*working.begin()] += endpoints[i].t - last;
         }
         if (!working.empty()) {
-            actualTimeCovered += endpoints[i].time - last;
+            actualTimeCovered += endpoints[i].t - last;
         }
         if (working.find(endpoints[i].index) != working.end()) {
             working.erase(endpoints[i].index);
         } else {
             working.insert(endpoints[i].index);
         }
-        last = endpoints[i].time;
+        last = endpoints[i].t;
     }
 
-    int maxCoveredIfFire = 0;
+    long maxCoveredIfFire = 0;
     for (long i = 0; i < N; i++) {
         if (actualTimeCovered - alone[i] > maxCoveredIfFire)
             maxCoveredIfFire = actualTimeCovered - alone[i];
