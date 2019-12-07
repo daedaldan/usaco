7245827740eb911385395c94a3fb6faddebca9f1
Daniel Wang <daniel22373@gmail.com>
2019-12-07 16:47:26 -0500

    Worked on Convention II

--- Practice/Contests/Silver/2018-2019/2018 December/Convention II/convention2.cpp
+++ Practice/Contests/Silver/2018-2019/2018 December/Convention II/convention2.cpp
@@ -19,6 +19,10 @@ struct cow {
 int N;
 cow cows[100000];
 priority_queue<cow> waiting;
+int maxWaitingTime = -1;
+bool firstCowArrived = false;
+cow samplingCow;
+int samplingEnd = -1;
 
 bool arrivalCmp (cow a, cow b) {
     return a.arrival < b.arrival;
@@ -40,11 +44,69 @@ int main() {
     fin.close();
 
     sort(cows, cows+N, arrivalCmp);
+    samplingEnd = cows[0].arrival;
+
+//    for (int i = 0; i < N; i++) {
+//        bool arrived = false;
+//        if (!(!waiting.empty() && cows[i].arrival > samplingEnd)) {
+//            cout << "ARRIVAL at " << cows[i].arrival << " with seniority " << cows[i].seniority << endl;
+//            waiting.push(cows[i]);
+//            arrived = true;
+//        }
+//
+//        if (cows[i].arrival >= samplingEnd || cows[i].arrival == samplingCow.arrival) {
+//            samplingCow = waiting.top();
+//            cout << "SAMPLING for cow that arrived at: " << samplingCow.arrival << " with seniority " << samplingCow.seniority << " who will sample for " << samplingCow.sampling << endl;
+//            int waitingTime = samplingEnd - samplingCow.arrival;
+//            cout << "    this cow waited for " << waitingTime << endl;
+//            if (waitingTime > maxWaitingTime) {
+//                maxWaitingTime = waitingTime;
+//            }
+//            samplingEnd = samplingEnd + samplingCow.sampling;
+//            waiting.pop();
+//            if (samplingEnd < 0)
+//                cout << "    sampling will end at " << 0 << endl;
+//            else
+//                cout << "    sampling will end at " << samplingEnd << endl;
+//        }
+//
+//        if (!arrived)
+//            i--;
+//    }
+
+    waiting.push(cows[0]);
+    int index = 1;
+    while (!waiting.empty()) {
+        if (!waiting.empty() && cows[index-1].arrival > samplingEnd) {
+            // new cow samples grass
+            samplingCow = waiting.top();
+            cout << "SAMPLING for cow that arrived at: " << samplingCow.arrival << " with seniority " << samplingCow.seniority << " who will sample for " << samplingCow.sampling << endl;
+            int waitingTime = samplingEnd - samplingCow.arrival;
+            cout << "    this cow waited for " << waitingTime << endl;
+            if (waitingTime > maxWaitingTime) {
+                maxWaitingTime = waitingTime;
+            }
+            samplingEnd = samplingEnd + samplingCow.sampling;
+            waiting.pop();
+            if (samplingEnd < 0)
+                cout << "    sampling will end at " << 0 << endl;
+            else
+                cout << "    sampling will end at " << samplingEnd << endl;
+        }
+
+        // another cow arrives
+        if ((waiting.empty() || cows[index].arrival <= samplingEnd) && index < N) {
+            cout << "ARRIVAL at " << cows[index].arrival << " with seniority " << cows[index].seniority << endl;
+            waiting.push(cows[index]);
+            index++;
+        }
+    }
+
 
     // writing output
     ofstream fout("convention2.out");
     if (fout.is_open()) {
-
+        fout << maxWaitingTime << "\n";
     } else cout << "error opening output file" << endl;
     fout.close();
 
