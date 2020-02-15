3b4ce810848b170563b4181faf6ce74736372e73
Daniel Wang <daniel22373@gmail.com>
2020-02-15 14:54:24 -0500

    Worked on Berry Picking

--- Practice/Contests/Silver/2019-2020/2020 January/Berry Picking/berries.cpp
+++ Practice/Contests/Silver/2019-2020/2020 January/Berry Picking/berries.cpp
@@ -1,3 +1,7 @@
+/*
+ * NOTE: This code is unfinished.
+ * */
+
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -6,7 +10,7 @@
 using namespace std;
 
 struct split {
-    int sum;
+    int sum = -1;
     int baskets[1000];
     bool solutionExists = true;
 };
@@ -14,43 +18,156 @@ struct split {
 int N, K;
 int trees[1000];
 int baskets[1000];
-int bottomSum = 1000001;
-
-/* try each possible split and return array and sum of optimal split */
-split checkOptions(int i) {
-    split s;
-    s.sum = 5;
-
-    return s;
-}
-
-int highLow() {
-    return 1;
-}
-
-int bothSame() {
-    return 1;
-}
-
-int bothHigh() {
-    return 1;
-}
-
-int bothLow() {
-    return 1;
-}
-
-int sameHigh() {
-    return 1;
-}
-
-int sameLow() {
-    return 1;
-}
 
 int copyArray(int original[], int copy[], int size) {
     for (int i = 0; i < size; i++)
         copy[i] = original[i];
+}
+
+/* one higher, one lower (but higher than lowest) */
+/* MULTIPLE POSSIBILITIES */
+split highLow(int i) {
+    split s;
+
+//    if () {
+//
+//    } else {
+//        s.solutionExists = false;
+//    }
+
+    s.solutionExists = false;
+
+    return s;
+}
+
+/* both equal to bound and greater than lowest */
+split bothSame(int i) {
+    split s;
+
+    if (baskets[i] / 2 == baskets[K/2 - 1] && baskets[i] / 2 > baskets[0]) {
+        int bound = baskets[K/2 - 1];
+        for (int i = K - 1; i > K/2; i--)
+            s.baskets[i] = baskets[i - 1];
+        for (int i = K/2 - 1; i > 0; i--)
+            s.baskets[i - 1] = baskets[i];
+        s.baskets[K/2 - 1] = bound;
+        s.baskets[K/2] = bound;
+
+        s.sum += 1;
+        for (int i = 0; i < K /2 ; i++)
+            s.sum += s.baskets[i];
+    } else {
+        s.solutionExists = false;
+    }
+
+    return s;
+}
+
+/* both higher than bound */
+/* MULTIPLE POSSIBILITIES */
+split bothHigh(int i) {
+    split s;
+
+//    if () {
+//        ;
+//    } else {
+//        s.solutionExists = false;
+//    }
+
+    s.solutionExists = false;
+
+    return s;
+}
+
+/* both lower than bound but one is greater than lowest and sum is greater than lowest + bound */
+/* MULTIPLE POSSIBILITIES */
+split bothLow(int i) {
+    split s;
+
+//    if () {
+//
+//    } else {
+//        s.solutionExists = false;
+//    }
+
+    s.solutionExists = false;
+
+    return s;
+}
+
+/* one same one higher and same is greater than min */
+split sameHigh(int i) {
+    split s;
+
+    if (baskets[K/2 - 1] > baskets[0] && baskets[i] > 2 * baskets[K/2 - 1]) {
+        int higher = baskets[i] - baskets[K/2 - 1];
+        s.baskets[i] = higher;
+        s.baskets[0] = baskets[K/2 - 1];
+        sort(s.baskets, s.baskets + K);
+    } else {
+        s.solutionExists = false;
+    }
+
+    return s;
+}
+
+/* one same one lower and lower is greater than min */
+split sameLow(int i) {
+    split s;
+
+    if (baskets[i] - baskets[K/2 - 1] > baskets[0]) {
+        int lower = baskets[i] - baskets[K/2 - 1];
+        s.baskets[0] = lower;
+        s.baskets[i] = baskets[K/2 - 1];
+        sort(s.baskets, s.baskets + K);
+    } else {
+        s.solutionExists = false;
+    }
+
+    return s;
+}
+
+/* try each possible split and return array and sum of optimal split */
+split checkOptions(int i) {
+    split s;
+
+    split hl = highLow(i);
+    if (hl.solutionExists && hl.sum > s.sum) {
+        s.sum = hl.sum;
+        copyArray(hl.baskets, s.baskets, K);
+    }
+
+    split bs = bothSame(i);
+    if (bs.solutionExists && bs.sum > s.sum) {
+        s.sum = bs.sum;
+        copyArray(bs.baskets, s.baskets, K);
+    }
+
+    split bh = bothHigh(i);
+    if (bh.solutionExists && bh.sum > s.sum) {
+        s.sum = bh.sum;
+        copyArray(bh.baskets, s.baskets, K);
+    }
+
+    split bl = bothLow(i);
+    if (bl.solutionExists && bl.sum > s.sum) {
+        s.sum = bl.sum;
+        copyArray(bl.baskets, s.baskets, K);
+    }
+
+    split sh = sameHigh(i);
+    if (sh.solutionExists && sh.sum > s.sum) {
+        s.sum = sh.sum;
+        copyArray(sh.baskets, s.baskets, K);
+    }
+
+    split sl = sameLow(i);
+    if (sl.solutionExists && sl.sum > s.sum) {
+        s.sum = sl.sum;
+        copyArray(sl.baskets, s.baskets, K);
+    }
+
+    return s;
 }
 
 int main() {
@@ -82,32 +199,43 @@ int main() {
             baskets[i] = trees[i];
     }
 
-//    bool done = false;
-//    while (!done) {
-//        done = true;
-//        int copyBaskets[1000];
-//        copyArray(baskets, copyBaskets, K);
-//
-//        int optimalSum = -1;
-//        int optimalBaskets[1000];
-//        for (int i = K/2; i < K; i++) {
-//            split result = checkOptions(i);
-//            if (result.solutionExists) {
-//                if (result.sum > optimalSum) {
-//                    optimalSum = result.sum;
-//                    copyArray(optimalBaskets, result.baskets, K);
-//                    done = false;
-//                }
-//            }
-//        }
-//
-//        copyArray(optimalBaskets, baskets, K);
-//    }
+    bool done = false;
+    int optimalSum = 0;
+    for (int i = 0; i < K/2; i++)
+        optimalSum += baskets[i];
+
+    while (!done) {
+        for (int i = 0; i < K; i++) {
+            if (i != K - 1)
+                cout << baskets[i] << ", ";
+            else
+                cout << baskets[i] << endl;
+        }
+        cout << optimalSum << endl;
+
+        done = true;
+        int copyBaskets[1000];
+        copyArray(baskets, copyBaskets, K);
+
+        int optimalBaskets[1000];
+        for (int i = K/2; i < K; i++) {
+            split result = checkOptions(i);
+            if (result.solutionExists) {
+                if (result.sum > optimalSum) {
+                    optimalSum = result.sum;
+                    copyArray(result.baskets, optimalBaskets, K);
+                    done = false;
+                }
+            }
+        }
+
+        copyArray(optimalBaskets, baskets, K);
+    }
 
     // writing output
     ofstream fout("berries.out");
     if (fout.is_open()) {
-
+        fout << optimalSum << "\n";
     } else cout << "error opening output file" << endl;
     fout.close();
 
