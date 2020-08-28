c063df3e23951c625be8c718aea89561d0d76427
Daniel Wang <daniel22373@gmail.com>
2020-08-28 18:51:41 -0400

    Solve Barn Repair (again)

--- Practice/Training Gateway/Chapter 1/Section 1.4/Barn Repair/barn1_practice.cpp
+++ Practice/Training Gateway/Chapter 1/Section 1.4/Barn Repair/barn1_practice.cpp
@@ -7,21 +7,66 @@ LANG: C++
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <algorithm>
 
 using namespace std;
 
+struct gap {
+    int start;
+    int end;
+};
+
+int M, S, C;
+int stalls[200];
+int cows[200];
+gap gaps[200];
+int numBoards = 1;
+int covered = 0;
+
+bool cmp(gap a, gap b) { return a.end - a.start > b.end - b.start; }
+
 int main() {
     // reading input
-    ifstream fin("input.txt");
+    ifstream fin("barn1.in");
     if (fin.is_open()) {
-
+        fin >> M >> S >> C;
+        for (int i = 0; i < C; i++) {
+            fin >> cows[i];
+        }
     } else cout << "error opening input file" << endl;
     fin.close();
 
-    // writing output
-    ofstream fout("output.txt");
-    if (fout.is_open()) {
+    sort(cows, cows + C);
 
+    for (int i = cows[0] - 1; i < cows[C - 1]; i++) {
+        stalls[i] = 1;
+    }
+
+    for (int i = 0; i < C - 1; i++) {
+        gaps[i].start = cows[i];
+        gaps[i].end = cows[i + 1];
+    }
+
+    sort(gaps, gaps + C - 1, cmp);
+
+    while (numBoards < M) {
+        for (int i = gaps[numBoards - 1].start; i < gaps[numBoards - 1].end - 1; i++) {
+            stalls[i] = 0;
+        }
+
+        numBoards++;
+    }
+
+    for (int i = 0; i < S; i++) {
+        if (stalls[i]) {
+            covered++;
+        }
+    }
+
+    // writing output
+    ofstream fout("barn1.out");
+    if (fout.is_open()) {
+        fout << covered << "\n";
     } else cout << "error opening output file" << endl;
     fout.close();
 
